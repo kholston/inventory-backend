f2e9c45a703640257c95d48e1b022507eddd0a2c
Ken Holston <git.kholston@gmail.com>
2021-08-30 18:52:57 -0400

    setup linting and basic server

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import http from 'http'
+import logger from './utils/logger'
+import config from './utils/config'
+import app from './app'
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+	logger.info(`Server running on port ${config.PORT}`)
+})
---
+++ src/tests/server.test.js
@@ -0,0 +1,14 @@
+import supertest from 'supertest'
+import app from '../app'
+
+const server = supertest(app)
+
+describe('server starts', () => {
+  test.skip('server returns basic message', () => {
+    server.get('/')
+      .expect(200)
+      .then(response => {
+        expect(response.text).toContain('Server Running')
+      })
+  })
+})
